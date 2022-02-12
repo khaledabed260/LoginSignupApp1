30c24928c321c10d15a99b826296f98164702dc9
Khaled Abed <khaledabed2601@gmail.com>
2022-02-12 10:04:19 +0200

    12.2

--- app/src/main/java/com/example/loginsignupapp/AdapterWorkout.java
+++ app/src/main/java/com/example/loginsignupapp/AdapterWorkout.java
@@ -1,26 +1,46 @@
 package com.example.loginsignupapp;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AdapterWorkout extends RecyclerView.Adapter<AdapterWorkout.ViewHolder> {
 
     private List<HomeWorkout> mData;
     private LayoutInflater mInflater;
-    private AdapterWorkout.ItemClickListener mClickListener;
+    private Context context;
+
+
+    private final AdapterWorkout.ItemClickListener mClickListener = new ItemClickListener() {
+        @Override
+        public void onItemClick(View view, int position) {
+            // get restaurant data
+            HomeWorkout workout = mData.get(position);
+            // upload restaurant data
+            // goto details activity
+            Intent i = new Intent(context, WorkoutDetailsActivity.class);
+            i.putExtra("workout", (Serializable)workout);
+            context.startActivity(i);
+        }
+    };
 
     // data is passed into the constructor
     AdapterWorkout(Context context, List<HomeWorkout> data) {
         this.mInflater = LayoutInflater.from(context);
         this.mData = data;
+        this.context = context;
     }
 
     // inflates the row layout from xml when needed
@@ -33,8 +53,8 @@ public class AdapterWorkout extends RecyclerView.Adapter<AdapterWorkout.ViewHold
     // binds the data to the TextView in each row
     @Override
     public void onBindViewHolder(AdapterWorkout.ViewHolder holder, int position) {
-        HomeWorkout rest = mData.get(position);
-        holder.tvName.setText(rest.getName());
+        HomeWorkout workout = mData.get(position);
+        holder.tvName.setText(workout.getName());
         //holder.ivPhoto.setImageDrawable(rest.getPhoto());
     }
 
@@ -64,14 +84,15 @@ public class AdapterWorkout extends RecyclerView.Adapter<AdapterWorkout.ViewHold
     }
 
     // convenience method for getting data at click position
-    HomeWorkout getItem(int id) {
+  HomeWorkout getItem(int id) {
         return mData.get(id);
     }
 
     // allows clicks events to be caught
-    void setClickListener(AdapterWorkout.ItemClickListener itemClickListener) {
+    /*
+    void setClickListener(AdapterRestaurant.ItemClickListener itemClickListener) {
         this.mClickListener = itemClickListener;
-    }
+    }*/
 
     // parent activity will implement this method to respond to click events
     public interface ItemClickListener {
